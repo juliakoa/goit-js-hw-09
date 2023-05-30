668bef0ff8c5c6bd9004465c008dd8eafa024c9e
juliakoa <julisia199706@gmail.com>
2023-05-30 17:35:13 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.body;
+let intervalId = null;
 
+startButton.addEventListener('click', startChangingColor);
+stopButton.addEventListener('click', stopChangingColor);
+
+function startChangingColor() {
+  startButton.disabled = true;
+  intervalId = setInterval(changeBackgroundColor, 1000);
+}
+
+function stopChangingColor() {
+  startButton.disabled = false;
+  clearInterval(intervalId);
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
